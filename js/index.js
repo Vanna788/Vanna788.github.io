106399e88eadb5174b593daf03a903831f996f99
Vanna788 <hal20024@byui.edu>
2020-09-18 11:30:36 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
+// Modified date
 let lastdate = document.lastModified;
 document.getElementById("date").innerHTML = "Last updated: " + lastdate;
+// The year
+let d = new Date();
+let y = d.getFullYear();
+document.getElementById("year").innerHTML = y;
