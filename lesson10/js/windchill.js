f35d085f8c41924ee08a874d15f70e1af527a9d5
Vanna788 <hal20024@byui.edu>
2020-11-21 09:38:06 -0600

    update

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,9 +1,9 @@
 
 const tempnumber = parseFloat(document.getElementById("ht").textContent);
-//console.log(tempnumber);
+console.log(tempnumber);
 
 const speedNumber = parseFloat(document.getElementById("ws").textContent);
-//console.log(speedNumber);
+console.log(speedNumber);
 
 let windchill = 35.74 + (0.6215 * tempnumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempnumber * Math.pow(speedNumber, 0.16));
 windchill = Math.round(windchill);
