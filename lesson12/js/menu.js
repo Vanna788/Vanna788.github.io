4ad6fa32d1e539751c90e46e75721624bc777453
Vanna788 <hal20024@byui.edu>
2020-12-13 12:57:17 -0600

    update

--- lesson12/js/menu.js
+++ lesson12/js/menu.js
@@ -8,5 +8,16 @@ function menuTab() {
 let lastdate = document.lastModified;
 document.getElementById("date").innerHTML = "Last updated: " + lastdate;
 
-  
+  // price!
+
+  const URL = "https://vanna788.github.io/lesson12/data/prices.json";
+
+  fetch(URL)
+  .then((response) => response.json())
+  .then((rentalObj) => {
+     console.log(rentalObj);
+
+     document.getElementById('price').innerHTML = rentalObj.rentals[0].resHalfday + '$';
+
+  });
   
